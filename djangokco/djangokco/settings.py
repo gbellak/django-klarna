84fbd37c91fabadce1db2010d4d1e9af9283a252
Gabor Bellak <gbellak11@gmail.com>
2019-01-18 17:51:56 +0100

    first commit

--- djangokco/djangokco/settings.py
+++ djangokco/djangokco/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'djangokco.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangokco',
+        'USER': 'django_user',
+        'PASSWORD': 'djangouserpw',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
